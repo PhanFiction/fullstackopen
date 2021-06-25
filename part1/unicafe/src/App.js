1b680717fac9e686bbc4b68737463afaeafe4796
Steve Wu <stevewu24@gmail.com>
2021-06-25 13:35:09 -0700

    finished 1.7-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
 import React, {useState} from 'react';
+
+function Statistic({value, text})
+{
+  return(
+    <tr>
+      <td> {text} {value} </td>
+    </tr>
+  );
+}
+
+function Button(props)
+{
+  return(
+    <button onClick={props.handleClick}>
+      {props.children}
+    </button>
+  )
+}
+
+function Statistics(props)
+{
+  const {good, bad, neutral, all, avg, positive} = props;
+  if(good === 0 && bad === 0 && neutral === 0) 
+  {
+    return(
+      <h2 id="nofeedback"> no feedback given </h2>
+    );
+  }
+  return(
+    <table>
+      <tbody>
+        <Statistic text={'good'} value={good}/>
+        <Statistic text={'neutral'} value={neutral}/>
+        <Statistic text={'bad'} value={bad}/>
+        <Statistic text={'all'} value={all}/>
+        <Statistic text={'average'} value={avg}/>
+        <Statistic text={'positive'} value={positive}/>
+      </tbody>
+    </table>
+  )
+}
 
 // lifting state up
 export default function App() {
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const avg = (good - bad) / all;
+  const positive = (good / all) * 100+ '%';
 
   const handleBad = () =>{
     setBad(bad + 1);
@@ -16,40 +61,17 @@ export default function App() {
   }
 
   const handleNeutral = () =>{
-    setNeutral(neutral + 1);
+    setNeutral(neutral+1);
   }
   
   return (
-    <div>
-      <h1>give feedback</h1>
-      <div className="button-container">
-        <Button handleClick={handleGood}>good</Button>
-        <Button handleClick={handleBad}>bad</Button>
-        <Button handleClick={handleNeutral}>neutral</Button>
-      </div>
-      <h1>statistics</h1>
-      <Display number={good}> good </Display> 
-      <Display number={bad}> bad </Display> 
-      <Display number={neutral}> neutral </Display>  
-    </div>
+    <>
+      <h1> give feedback</h1>
+      <Button handleClick={handleGood}>good</Button>
+      <Button handleClick={handleBad}>bad</Button>
+      <Button handleClick={handleNeutral}>neutral</Button>
+      <h1> statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} avg={avg} positive={positive}/>
+      </>
   );
 }
-
-function Display(props)
-{
-  const {children} = props;
-  return(
-    <p>
-      {children} {props.number}
-    </p>
-  );
-}
-function Button(props)
-{
-  return(
-    <button onClick={props.handleClick}>
-      {props.children}
-    </button>
-  )
-}
-
